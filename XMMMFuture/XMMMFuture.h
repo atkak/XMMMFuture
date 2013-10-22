317ffdfb167099793b663003030343e0a7917a59
xiongmaomaomao <pasapasa.va1104@gmail.com>
2013-10-22 23:18:09 +0900

    Initial Commit

---
+++ XMMMFuture/XMMMFuture.h
@@ -0,0 +1,13 @@
+//
+//  XMMMFuture.h
+//  XMMMFuture
+//
+//  Created by KAKEGAWA Atsushi on 2013/10/22.
+//  Copyright (c) 2013年 KAKEGAWA Atsushi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMMMFuture : NSObject
+
+@end
---
+++ XMMMFuture/XMMMFuture.m
@@ -0,0 +1,13 @@
+//
+//  XMMMFuture.m
+//  XMMMFuture
+//
+//  Created by KAKEGAWA Atsushi on 2013/10/22.
+//  Copyright (c) 2013年 KAKEGAWA Atsushi. All rights reserved.
+//
+
+#import "XMMMFuture.h"
+
+@implementation XMMMFuture
+
+@end
