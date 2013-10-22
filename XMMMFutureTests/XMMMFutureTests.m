20de169a901ec1887e2e2526be74a2709f0f9c4f
xiongmaomaomao <pasapasa.va1104@gmail.com>
2013-10-23 01:03:03 +0900

    Implement map and flatMap

--- XMMMFutureTests/XMMMFutureTests.m
+++ XMMMFutureTests/XMMMFutureTests.m
@@ -12,6 +12,8 @@
 
 @interface XMMMFutureTests : XCTestCase
 
+@property (atomic) BOOL completed;
+
 @end
 
 @implementation XMMMFutureTests
@@ -19,12 +21,16 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    self.completed = NO;
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    do {
+        [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+    } while (!self.completed);
+    
     [super tearDown];
 }
 
@@ -33,6 +39,8 @@
     XMMMPromise *promise = [XMMMPromise promise];
     
     XCTAssertNotNil(promise, @"Instance should not be nil.");
+
+    self.completed = YES;
 }
 
 - (void)testFutureCreation
@@ -41,6 +49,8 @@
     XMMMFuture *future = promise.future;
     
     XCTAssertNotNil(future, @"Future should not be nil.");
+    
+    self.completed = YES;
 }
 
 - (void)testResolve
@@ -58,7 +68,11 @@
         XCTFail(@"Failure block should not be called.");
     }];
     
-    [promise resolve:obj1];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [promise resolve:obj1];
+        self.completed = YES;
+    });
+    
 }
 
 - (void)testReject
@@ -67,6 +81,10 @@
     XMMMFuture *future = promise.future;
     
     NSError *error1 = [NSError errorWithDomain:@"test" code:1 userInfo:nil];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [promise reject:error1];
+        self.completed = YES;
+    });
     
     [future success:^(id result) {
         XCTFail(@"Success block should not be called.");
@@ -75,8 +93,120 @@
     [future failure:^(NSError *error) {
         XCTAssertEqual(error, error1, @"Error object should be same as rejected one.");
     }];
-    
-    [promise reject:error1];
 }
+
+- (void)testMap
+{
+    XMMMPromise *promise = [XMMMPromise promise];
+    XMMMFuture *future1 = promise.future;
+    
+    XMMMFuture *future2 = [future1 map:^id(id result) {
+        return [result stringByAppendingString:@", world!"];
+    }];
+    
+    XCTAssertNotNil(future2, @"Mapped Future should not be nil.");
+    
+    NSString *str1 = @"Hello";
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [promise resolve:str1];
+        self.completed = YES;
+    });
+    
+    [future2 success:^(id result) {
+        XCTAssertEqualObjects(result, @"Hello, world!", @"");
+    }];
+}
+
+- (void)testMapFailed
+{
+    XMMMPromise *promise = [XMMMPromise promise];
+    XMMMFuture *future1 = promise.future;
+    
+    XMMMFuture *future2 = [future1 map:^id(id result) {
+        XCTFail(@"");
+        return nil;
+    }];
+    
+    XCTAssertNotNil(future2, @"Mapped Future should not be nil.");
+    
+    NSError *error1 = [NSError errorWithDomain:@"test" code:1 userInfo:nil];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [promise reject:error1];
+        self.completed = YES;
+    });
+    
+    [future2 success:^(id result) {
+        XCTFail(@"");
+    }];
+    
+    [future2 failure:^(NSError *error) {
+        XCTAssertEqual(error, error1, @"");
+    }];
+}
+
+- (void)testFlatMap
+{
+    XMMMPromise *promise1 = [XMMMPromise promise];
+    XMMMFuture *future1 = promise1.future;
+    
+    NSString *str1 = @"Hello";
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [promise1 resolve:str1];
+    });
+    
+    XMMMFuture *composedFuture = [future1 flatMap:^XMMMFuture *(id result) {
+        XMMMPromise *promise2 = [XMMMPromise promise];
+        XMMMFuture *future2 = promise2.future;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [promise2 resolve:[result stringByAppendingString:@", world!"]];
+        });
+        
+        return future2;
+    }];
+    
+    [composedFuture success:^(id result) {
+        XCTAssertEqualObjects(result, @"Hello, world!", @"");
+        self.completed = YES;
+    }];
+    
+    [composedFuture failure:^(NSError *error) {
+        XCTFail(@"");
+        self.completed = YES;
+    }];
+}
+
+- (void)testFlatMapFirstFutureFailed
+{
+    XMMMPromise *promise1 = [XMMMPromise promise];
+    XMMMFuture *future1 = promise1.future;
+    
+    NSError *error1 = [NSError errorWithDomain:@"test" code:1 userInfo:nil];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [promise1 reject:error1];
+    });
+    
+    XMMMFuture *composedFuture = [future1 flatMap:^XMMMFuture *(id result) {
+        XMMMPromise *promise2 = [XMMMPromise promise];
+        XMMMFuture *future2 = promise2.future;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [promise2 resolve:[result stringByAppendingString:@", world!"]];
+        });
+        
+        return future2;
+    }];
+    
+    [composedFuture success:^(id result) {
+        XCTFail(@"");
+        self.completed = YES;
+    }];
+    
+    [composedFuture failure:^(NSError *error) {
+        XCTAssertEqualObjects(error, error1, @"");
+        self.completed = YES;
+    }];
+}
+
 
 @end
