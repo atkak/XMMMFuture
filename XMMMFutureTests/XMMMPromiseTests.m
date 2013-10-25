5db45a02a7f1071979e7b8f09551775c8d4b9b37
xiongmaomaomao <pasapasa.va1104@gmail.com>
2013-10-25 11:14:38 +0900

    Move test methods for appropriate class

--- XMMMFutureTests/XMMMPromiseTests.m
+++ XMMMFutureTests/XMMMPromiseTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "XMMMPromise.h"
 
 @interface XMMMPromiseTests : XCTestCase
 
@@ -17,13 +18,26 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here; it will be run once, before the first test case.
+    
 }
 
 - (void)tearDown
 {
-    // Put teardown code here; it will be run once, after the last test case.
+    
     [super tearDown];
+}
+
+#pragma mark - Test methods
+
+- (void)testCreation
+{
+    XMMMPromise *promise = [XMMMPromise defaultPromise];
+    
+    XCTAssertNotNil(promise, @"Promise should not be nil.");
+    
+    XMMMFuture *future = promise.future;
+    
+    XCTAssertNotNil(future, @"Future should not be nil.");
 }
 
 @end
